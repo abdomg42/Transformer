e219913ee24524040bcb5e1387318d37945a783f
abdomg42 <sonysam3a@gmail.com>
2026-02-12 14:53:48 +0100

    add Transformer

--- model.py
+++ model.py
@@ -147,3 +147,37 @@ class Decoder(nn.Module):
         for layer in self.layers:
             x = layer(x, encoder_output, src_mask, tgt_mask)
         return self.norm(x)
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model: int, vocab_size:int):
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+    
+    def forward(self, x):
+        return torch.log_softmax(self.proj(x), dim=-1)
+    
+class Transformer(nn.Module):
+
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: inputEmbeddings, tgt_embed: inputEmbeddings,src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_emb = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+        def encoder(self, src, src_mask):
+            src = self.src_embed(src)
+            src = self.src_pos(src)
+            return self.encoder(src, src_mask)
+        
+        def decoder(self,encoder_output, src_mask, tgt, tgt_mask):
+            tgt = self.tgt_embed(tgt)
+            tgt = self.tgt_pos(tgt)
+            return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+        def project(self, x):
+            return self.projection_layer(x)
+
+        
